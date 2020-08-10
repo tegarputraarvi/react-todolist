40f2ddf2ee639821b85c5760157c24be5a2679bc
tegarputraarvi <tegarputraarvi.e@gmail.com>
2020-08-10 11:00:26 +0700

    Create styling container adn frame

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './home.css';
+
+class Home extends Component {
+
+    render = () => {
+        return (
+            <div className="container">
+                <div className="frame"></div>
+            </div>
+        ); 
+    }
+}
+
+export default Home;
