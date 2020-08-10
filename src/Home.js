880727e7ca6bf6070235a97e0fe5b1cfc27111f4
tegarputraarvi <tegarputraarvi.e@gmail.com>
2020-08-10 11:24:25 +0700

    Add header section

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,13 @@ class Home extends Component {
     render = () => {
         return (
             <div className="container">
-                <div className="frame"></div>
+                <div className="frame">
+                    <div className="header">
+                        <button className="header-btn">Add</button>
+                        <h1 className="header-title">Todo List</h1>
+                        <button className="header-btn main-red-color">Clear</button>
+                    </div>
+                </div>
             </div>
         ); 
     }
