85ca8274fa5ac837ee72bffc1d19a0b0f36ae8d8
tegarputraarvi <tegarputraarvi.e@gmail.com>
2020-08-13 10:26:25 +0700

    Button Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Button from '../components/Button';
+
 const Header = ({ showAddToggle, showAdd, clearTodos })  => {
     return (
         <section className="header">
-            <button className="header-btn" onClick={ showAddToggle }>
-            { showAdd ? 'Finish' : 'Add' }
-            </button>
+            <Button text={ showAdd ? 'Finish' : 'Add' } onClick={ showAddToggle } />
             <h1 className="header-title">Todo List</h1>
-            <button className="header-btn main-red-color" onClick={ clearTodos }>Clear</button>
+            <Button text="Clear" onClick={ clearTodos } color="red" align="right" />
         </section>
     );
 };
